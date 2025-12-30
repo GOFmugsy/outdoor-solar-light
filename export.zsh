9e6c3f3e29e592005a44debdfa20ac779111325b
GOFmugsy <jeff.monroe@gmail.com>
2025-12-30 18:46:22 -0500

    fix export script

--- export.zsh
+++ export.zsh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 hash=`git rev-parse --short HEAD`
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o neopixel-rpi-light-ring-$hash.stl case.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o solar-neopixel-holder-$hash.stl case.scad
