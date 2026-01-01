336d957c0fb0a0b38816904e98605ca240328b1b
GOFmugsy <jeff.monroe@gmail.com>
2026-01-01 16:30:14 -0500

    add bottom

--- export.zsh
+++ export.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 hash=`git rev-parse --short HEAD`
-/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o solar-neopixel-holder-$hash.stl case.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o solar-neopixel-holder-$hash.stl solar-neo.scad
+/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o bottom-$hash.stl bottom.scad
